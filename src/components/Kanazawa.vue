ba15cff51efcafbc1fa68f86e616a8fbd9b34a1f
Rena-Okano <rena428928@gmail.com>
2021-06-22 22:55:03 +0900

    一部デザイン

--- src/components/Kanazawa.vue
+++ src/components/Kanazawa.vue
@@ -4,44 +4,54 @@
   <div id="wrapper">
     <div id="resultBlock">
       <div id="resultAreaBlock" class="Abidjan">
-          <div id=resultAreaTitle>"あなたにおすすめの旅先は...”
-              <br>
-              <h2>アビジャン</h2>
-          </div>
-          <div></div>
-      <div id="resultAreaBody">
-      "〜がおすすめ"
-      <br>
-      "〜も素敵なアビジャン"
+        <div id="resultAreaTitle">
+          "あなたにおすすめの旅先は...”
+          <br />
+          <h2>金沢</h2>
+        </div>
+        <div></div>
+        <div id="resultAreaBody">
+          "〜がおすすめ"
+          <br />
+          "〜も素敵なアビジャン"
+        </div>
+        <div id="resultAreaImage" class="sp_on">
+          <img src="../assets/Abidjan_top.jpg" alt="アビジャン" />
+        </div>
+      </div>
+      <div id="resultImageBlock">
+        <div class="image_thumb">
+          <img src="../assets/Abidjan_聖ポール大聖堂.jpg" alt="観光地1" />
+          <p>聖ポール大聖堂</p>
+          <p>個性的なアフリカ模様のステンドグラスが見られるモダンな大聖堂</p>
+        </div>
+        <div class="image_thumb">
+          <img src="../assets/Abidjan_プラトーモスク.jpg" alt="観光地2" />
+          <p>プラトーのグランドモスク</p>
+          <p>
+            イスラム教徒の祈りの場、絵本の世界から飛び出たようなデザインの可愛らしいモスク
+          </p>
+        </div>
+        <div class="image_thumb">
+          <img src="../assets/Abidjan_Tai-National-Park.jpg" alt="観光地3" />
+          <p>タイ国立公園</p>
+          <p>
+            西アフリカ最後の貴重な原生林が広がる！コートジボワールの世界遺産
+          </p>
+        </div>
+      </div>
+      <div id="resultTxtBlock">
+        "〜〜〜〜〜〜〜〜〜〜〜〜〜〜"
+        <br />
+        "〜〜〜〜〜〜〜〜〜〜〜〜〜"
+      </div>
+      <div id="resultLinkBlock" class="clearfix">
+        <div id="snsBlock" class="clearfix">
+          <a href=" ">
+            <img src=" " alt="もう一度診断する" />
+          </a>
+        </div>
+      </div>
     </div>
-    <div id="resultAreaImage" class="sp_on">
-        <img src="" alt="アビジャン">
-    </div>
-  </div>
-  <div id="resultImageBlock">
-      <div class="image_thumb">
-          <img src="" alt="観光地1">
-          <p>観光地1</p>
-      </div>
-      <div class="image_thumb">
-          <img src="" alt="観光地2">
-          <p>観光地2</p>
-      </div>
-      <div class="image_thumb">
-          <img src="" alt="観光地3">
-          <p>観光地3</p>
-      </div>
-  </div>
-  <div id="resultTxtBlock">
-      "〜〜〜〜〜〜〜〜〜〜〜〜〜〜"
-      <br>
-      "〜〜〜〜〜〜〜〜〜〜〜〜〜"
-  </div>
-  <div id="resultLinkBlock" class="clearfix">
-  <div id=snsBlock class="clearfix">
-      <a href=" ">
-          <img src=" " alt="もう一度診断する">
-      </a>
-  </div>
   </div>
 </template>
