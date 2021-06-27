8c4b8d810a35cada20d71d32d69a6054c5a253e0
akskoon <akisaki.11to5@icloud.com>
2021-06-27 20:33:45 +0900

    cssを変更

--- src/components/Abidjan.vue
+++ src/components/Abidjan.vue
@@ -5,13 +5,14 @@
     <div id="resultBlock">
       <div id="resultAreaBlock" class="Abidjan">
         <div id="resultAreaTitle">
-          "あなたにおすすめの旅先は...”
+          あなたにおすすめの旅先は...
           <br />
           <h2>アビジャン</h2>
         </div>
         <div></div>
         <div id="resultAreaBody">
           "西アフリカ コートジボワールの大都市！"
+          <br />
           <br />
           "近未来建築と大自然が共存するアビジャン"
         </div>
@@ -58,15 +59,16 @@
           </div>
         </div>
       </div>
-      <div id="resultTxtBlock">
+      <!-- <div id="resultTxtBlock">
         "〜〜〜〜〜〜〜〜〜〜〜〜〜〜"
         <br />
         "〜〜〜〜〜〜〜〜〜〜〜〜〜"
-      </div>
+      </div> -->
       <div id="resultLinkBlock" class="clearfix">
         <div id="snsBlock" class="clearfix">
-          <a href=" ">
-            <img src=" " alt="もう一度診断する" />
+          <div class=""></div>
+          <a href=" " class="retry-btn">
+            <span>もう一度診断する</span>
           </a>
         </div>
       </div>
@@ -74,45 +76,86 @@
   </div>
 </template>
 
-<style>
+<style scoped>
 /* resultBlock */
 .wrapper {
   width: 100%;
+}
+
+h2 {
+  position: relative;
+  padding: 1em 2em;
+  text-align: center;
+}
+
+h2:before,
+h2:after {
+  position: absolute;
+  content: "";
+}
+
+h2:after {
+  top: 0;
+  left: 0;
+  width: 50px;
+  height: 50px;
+  border-top: 2px solid rgb(248, 233, 233);
+  border-left: 2px solid rgb(248, 233, 233);
+}
+
+h2:before {
+  right: 0;
+  bottom: 0;
+  width: 50px;
+  height: 50px;
+  border-right: 2px solid rgb(248, 233, 233);
+  border-bottom: 2px solid rgb(248, 233, 233);
 }
 
 .wrapper #resultBlock {
   max-width: 1000px;
   margin: 0px auto;
   padding: 80px 0px;
+  font-family: "Hannotate SC";
 }
 /* resultImageBlock */
-/* .wrapper #resultAreaBlock #resultAreaTitle {
-  margin: 0px 0px 40px 0px;
+#resultAreaBlock {
+  margin: 40px 0px 40px 0px;
   font-size: 24px;
   font-weight: normal;
   text-align: center;
-} */
+}
+#resultAreaImage {
+  margin: 70px 0px 70px 0px;
+}
+
 @media (min-width: 992px) {
   .wrapper #resultAreaBlock #resultImageBlock {
     display: flex;
     align-items: center;
     justify-content: center;
   }
+  #resultImageBlock {
+    margin-top: 70px;
+    margin-bottom: 70px;
+  }
 }
-.wrapper #resultAreaBlock .image_thumb {
+.image_thumb {
   margin: 50px 0px;
   padding: 0px 50px;
   text-align: center;
+  float: left;
 }
 @media (min-width: 992px) {
-  .wrapper #resultAreaBlock .image_thumb {
+  .image_thumb {
     margin: 0px;
     width: calc(100% / 3);
   }
 }
-.wrapper #resultAreaBlock .image_thumb .image {
+.image_thumb .image {
   display: block;
-  width: 100%;
+  width: 300px;
+  height: 300px;
   max-width: 300px;
   margin: 0px auto 20px auto;
   border-radius: 100%;
@@ -121,5 +164,65 @@
   line-height: 1.8rem;
   font-size: 0.8rem;
   color: #666666;
+}
+
+.retry-btn {
+  /*その他と主な共通部分は省略*/
+
+  overflow: hidden;
+  margin-top: 50rem;
+  padding: 1.5rem 6rem;
+
+  color: #fff;
+  border-radius: 0;
+  background: #000;
+}
+
+.retry-btn span {
+  position: relative;
+  z-index: 1;
+}
+
+.retry-btn:before {
+  position: absolute;
+  top: 0;
+  left: calc(-100% + 10px);
+
+  width: 100%;
+  height: 100%;
+
+  content: "";
+  -webkit-transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-in-out;
+  -webkit-transform: translateX(0) translateY(0);
+  transform: translateX(0) translateY(0);
+
+  background: #094;
+}
+
+.retry-btn:after {
+  position: absolute;
+  top: 0;
+  right: calc(-100% + 10px);
+
+  width: 100%;
+  height: 100%;
+
+  content: "";
+  -webkit-transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-in-out;
+  -webkit-transform: translateX(0) translateY(0);
+  transform: translateX(0) translateY(0);
+
+  background: #094;
+}
+
+.retry-btn:hover:before {
+  -webkit-transform: translateX(50%) translateY(0);
+  transform: translateX(50%) translateY(0);
+}
+.retry-btn:hover:after {
+  -webkit-transform: translateX(-50%) translateY(0);
+  transform: translateX(-50%) translateY(0);
 }
 </style>
--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -191,7 +191,7 @@ export default {
   },
 }
 </script>
-<style>
+<style scoped>
 h1 {
   font-size: 50px;
   text-align: center;
